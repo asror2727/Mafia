41ccb1075808fa434fdfde3334f2ee33187b94fc
asror2727 <asrorake001@gmail.com>
2026-06-29 15:04:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import os
 from config import BOT_TOKEN
 from handlers import register_handlers
 
-bot = telebot.TeleBot(8840892305:AAExPeCpVhH5a4i46kPF9jPpIJvFZxxUGXc), parse_mode="HTML")
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
 
 register_handlers(bot)
 
 if __name__ == "__main__":
     print("🎭 Mafia Bot ishga tushdi...")
-    bot.infinity_polling(timeout=10, long_polling_timeout=5)
-
-if __name__ == "__main__":
-    print("🎭 Mafia Bot ishga tushdi...")
-    bot.remove_webhook()   # ← shu qatorni qo'shing
+    bot.remove_webhook()
     bot.infinity_polling(timeout=10, long_polling_timeout=5)
